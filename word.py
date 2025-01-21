665747db668dc9e61aa53a6b56965f6201be882c
Tharanie <tharaniemadhu@gmail.com>
2025-01-21 16:30:24 +0530

    updated

---
+++ word.py
@@ -0,0 +1,19 @@
+async def app(scope, receive, send):
+    # HTTP response headers
+    headers = [('Content-Type', 'text/html; charset=utf-8')]
+
+    # Start the HTTP response
+    assert scope['type'] == 'http'
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/html'],
+        ],
+    })
+
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, world!',
+    })
